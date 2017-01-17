d107eaa44b3c222ad019948a279b0263521861a9
Bernhard Rode <mail@bernhardrode.de>
2017-01-17 18:20:07 +0100

    fix: show full page title on mobile

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Titles; ?>
 
 <div class="row headline page-header">
-  <div class="col s8 offset-s4 right-align">
+  <div class="col s12 l8 offset-l4 right-align">
     <h1><?= Titles\title(); ?></h1>
   </div>
 </div>
